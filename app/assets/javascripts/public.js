44f1e7500d5940c8bddb69024924928f59b9ef09
Bay Gross <bay.gross@yale.edu>
2011-12-07 16:30:38 -0500

    new branding stuff and some results and about page aditions

---
+++ app/assets/javascripts/public.js
@@ -0,0 +1,14 @@
+//JS for public controller
+
+
+//About + Privacy Page
+$(document).ready(function(){
+
+    $('#container').css('height', '570px');
+
+    $('#mit-click').click(function(){
+        $('#mit').show();
+    });
+
+
+});
