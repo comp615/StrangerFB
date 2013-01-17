5d11ab185b4a6e2dbf5fcdc5a7d1489907c86e1e
Bay Gross <bgross17@gmail.com>
2013-01-16 22:25:26 -0500

    oops, end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,48 +4,48 @@ class ApplicationController < ActionController::Base
   before_filter :load_user
 
   def render_optional_error_file(status_code)
-     status = interpret_status(status_code)
-     render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'pages'
+    status = interpret_status(status_code)
+    render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'pages'
   end    
-  
-  
+
+
   protected
-  
+
   def set_local_vars
-      @current_action = action_name
-      @current_controller = controller_name
+    @current_action = action_name
+    @current_controller = controller_name
   end
-  
+
   def load_user  
-    
+
     #load fb_oauth object
-  	@fb_oauth = Koala::Facebook::OAuth.new(Facebook::APP_ID, Facebook::SECRET, Facebook::CALLBACK_URL) 
-    
+    @fb_oauth = Koala::Facebook::OAuth.new(Facebook::APP_ID, Facebook::SECRET, Facebook::CALLBACK_URL) 
+
     #if user is already logged in, just load graph object from session
     if session[:fb_id] && session[:fb_token]
       logger.error "Recreating graph object from session token"
       @fb_graph = Koala::Facebook::API.new( session[:fb_token] )
       return
     end
-    
-    
+
+
     #otherwise generate from cookies
     logger.error "Grabbing new graph token from login cookies"
-    
+
     #debug cookies first
     logger.error  "******* COOKIES ************" 
     logger.error cookies.to_json
-    
+
     #then get userid from cookies
     session[:fb_id] ||= @fb_oauth.get_user_from_cookies(cookies)
     logger.error  "******* FB ID ************" 
     logger.error session[:fb_id]
-    
+
     #and get user info from cookies
     fbdata = @fb_oauth.get_user_info_from_cookies(cookies)
     logger.error "******* FB Object ************"
     logger.error fbdata
-    
+
     #if no user or user info, something failed!
     if !session[:fb_id] || !fbdata
       @fb_graph = Koala::Facebook::API.new
@@ -54,15 +54,15 @@ class ApplicationController < ActionController::Base
     end
 
     #otherwise, continue
-    
+
     #grab access token
     token = fbdata["access_token"]
     logger.error "Token found: " + token
-    
+
     #and upgrade the graph object
     @fb_graph = Koala::Facebook::API.new( token )
     logger.error "Just upgraded graph object with token"
-    
+
     #check for user in DB 
     logger.error "Moving on to DB"
     @current_user = User.find_by_facebook_id( session[:fb_id] )
@@ -77,8 +77,5 @@ class ApplicationController < ActionController::Base
     @current_user.touch
   end
 
-    
-  end
-  
-  
+
 end
