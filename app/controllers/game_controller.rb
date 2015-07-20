809830d2b48ab1aa4a43f46feaf3fda25b1558cf
Charlie Croom <ccroom@twitter.com>
2015-07-19 18:47:40 -0700

    Test

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,5 @@
 class GameController < ApplicationController
-	
-	caches_page :splash
-	
+
   def splash
     #run animation on splash page
     @animatePhotosFlag = true
@@ -11,8 +9,8 @@ class GameController < ApplicationController
     # post an answer via AJAX
     # params :fb_id, :name => guess, :success
 
-    #get user data	
-    result = @fb_graph.fql_query("SELECT uid,name,sex,birthday_date,affiliations FROM user WHERE uid = " + params[:fb_id].to_s)   		
+    #get user data
+    result = @fb_graph.fql_query("SELECT uid,name,sex,birthday_date,affiliations FROM user WHERE uid = " + params[:fb_id].to_s)
 
     #fail if bad user
     if !result[0]["uid"]
@@ -26,14 +24,14 @@ class GameController < ApplicationController
       return
     end
 
-    # save attempt to DB 
-    @current_user.attempts.create({ 
+    # save attempt to DB
+    @current_user.attempts.create({
       :target_facebook_id => result[0]["uid"],
       :correct => params[:success],
-      :guessed_name => params[:name], 
+      :guessed_name => params[:name],
       :actual_name => result[0]["name"],
       :affiliations => ActiveSupport::JSON.encode(result[0]["affiliations"].map{|a| a["nid"].to_i}),
-      :gender => result[0]["sex"], 
+      :gender => result[0]["sex"],
       :age => User.age_from_bday_string( result[0]["birthday_date"] )
     })
     #return success
@@ -47,28 +45,28 @@ class GameController < ApplicationController
     #set defaults (if necessary) and convert to ints
     params[:limit] = params[:limit].to_i || 10
     params[:limit] = 10 if(params[:limit] > 15)
-    params[:avoid] = params[:avoid].map{|v| v.to_i} rescue [] 
+    params[:avoid] = params[:avoid].map{|v| v.to_i} rescue []
     puts "--AJAX call for  " + params[:limit].to_s + " friends."
-    
+
     #grab friends from facebook
     s = Time.now
     friends = @fb_graph.fql_query("SELECT uid,name FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())")
     puts "--Grab friends: " + (Time.now - s).to_s
-    
-    
+
+
     #Remove any repeats, and sample
     s = Time.now
     friends_in_db = @current_user.attempts.collect{|a| a.target_facebook_id}
     friends.reject! {|f| params[:avoid].include?(f["uid"].to_i) || friends_in_db.include?(f["uid"].to_i)}
     friends = friends.sample( params[:limit] )
-    
+
     if (friends.length == 0)
       puts "No friends left!"
       return nil
     end
     puts "--Select friends: " + (Time.now - s).to_s
-    
-    
+
+
     #Batch API to grab photos
     s = Time.now
     results = @fb_graph.batch do |batch_api|
@@ -79,14 +77,14 @@ class GameController < ApplicationController
     puts "--Batch grab photos: " + (Time.now - s).to_s
 
 
-   
+
     s = Time.now
     #loop over each friend block
     results.each_with_index do |friend, idx|
-      
+
       #for each friend, map over their photos, return new object with just necessary photo data
       photos = friend.map do |p|
-        
+
         #handle bug when no tags attachted for some reason
         if !p["tags"]
           { :xcoord => 0,
@@ -96,11 +94,11 @@ class GameController < ApplicationController
           }
         #normal procession
         else
-        
+
           all_tags = p["tags"]["data"]
           friend_id = friends[idx]["uid"].to_i
           tag = all_tags.detect{ |t| t["id"].to_i == friend_id}
-          
+
           { :xcoord =>  tag["x"],
             :ycoord =>  tag["y"],
             :src => p["source"],
@@ -108,7 +106,7 @@ class GameController < ApplicationController
           }
         end
       end
-      
+
       #sort by photos with fewest tags (most likely to be good face shot)
       friends[idx]["photos"] = photos.sort_by{|p| p[:total_tags]}.first(8).sample(4)
     end
@@ -116,7 +114,7 @@ class GameController < ApplicationController
 
 
     #format friend objects for javascript, and render response
-    friends = friends.map{|f| 
+    friends = friends.map{|f|
       {
         :fb_id => f['uid'],
         :name => f['name'],
@@ -125,8 +123,8 @@ class GameController < ApplicationController
       }
     }
     render :json => friends
-    
-    
+
+
     #Update play count if needed
     if (params[:new_round] == "true" )
       @current_user.play_count += 1
@@ -139,11 +137,11 @@ class GameController < ApplicationController
     @fullPageFlag = true
 
     #calculate aggregate stats
-    
+
     @overall_correct_pct = Rails.cache.fetch("overall", :expires_in => 7.days) do
       data = Attempt.connection.select_value("
-      	SELECT AVG(`correct`) 
-      	FROM 
+      	SELECT AVG(`correct`)
+      	FROM
       	(SELECT `user_id`, AVG(`correct`) as `correct`, COUNT(*) as `count`
 		        FROM `attempts` a
 		        WHERE `guessed_name` != 'unfair'
@@ -151,36 +149,36 @@ class GameController < ApplicationController
 		    ) `s`
       ")
 	  end
-      
+
 	  @gender_age_breakdown = Rails.cache.fetch("gender_age", :expires_in => 7.days) do
 	  	#recache
 	  	#break it down by age and genders
       Attempt.connection.select_all("
-	      SELECT AVG(`correct`) as `pct`,COUNT(*) as `count`,SUM(`count`) as `attempts`, s.`gender`,s.`age` 
-			FROM 
-			    (SELECT `user_id`, AVG(`correct`) as `correct`, COUNT(*) as `count`,u.`gender`,u.`age` 
-			        FROM `attempts` a INNER JOIN `users` u ON `u`.`id` = `a`.`user_id` 
+	      SELECT AVG(`correct`) as `pct`,COUNT(*) as `count`,SUM(`count`) as `attempts`, s.`gender`,s.`age`
+			FROM
+			    (SELECT `user_id`, AVG(`correct`) as `correct`, COUNT(*) as `count`,u.`gender`,u.`age`
+			        FROM `attempts` a INNER JOIN `users` u ON `u`.`id` = `a`.`user_id`
 			        WHERE `guessed_name` != 'unfair'
 			        GROUP BY `user_id`
-			    ) `s` 
-			GROUP BY `s`.`age`,`s`.`gender` 
+			    ) `s`
+			GROUP BY `s`.`age`,`s`.`gender`
 			ORDER BY `age` ASC")
-	  end	
-	  
+	  end
+
     @gender_gender_breakdown = Rails.cache.fetch("gender_gender", :expires_in => 7.days) do
       Attempt.connection.select_all("
-	      SELECT `user_gender`, s.`gender` as `gender`, AVG(`correct`) as `pct`, SUM(`count`) as `count` 
-	      FROM 
-			    (SELECT `user_id`, AVG(`correct`) as `correct`, COUNT(*) as `count`, u.`gender` as `user_gender`, a.`gender`, u.`age` 
-			        FROM `attempts` a INNER JOIN `users` u ON `u`.`id` = `a`.`user_id` 
+	      SELECT `user_gender`, s.`gender` as `gender`, AVG(`correct`) as `pct`, SUM(`count`) as `count`
+	      FROM
+			    (SELECT `user_id`, AVG(`correct`) as `correct`, COUNT(*) as `count`, u.`gender` as `user_gender`, a.`gender`, u.`age`
+			        FROM `attempts` a INNER JOIN `users` u ON `u`.`id` = `a`.`user_id`
 			        WHERE `guessed_name` != 'unfair'
 			        GROUP BY `user_id`, a.`gender`
 			    ) `s`
-	      GROUP BY `user_gender`, `gender` 
+	      GROUP BY `user_gender`, `gender`
 	      ORDER BY `user_gender` DESC, s.`gender` DESC")
 	  end
-      
-    #return here if not signed in 
+
+    #return here if not signed in
     @indivFlag = ( @current_user && !@current_user.attempts.blank? )
     return if !@indivFlag
 
@@ -194,18 +192,18 @@ class GameController < ApplicationController
       @curr_attempts = @current_user.attempts
     end
     @correct_attempts = @curr_attempts.select{|a| a.correct}
-    @incorrect_attempts = @curr_attempts.select{|a| !a.correct}		
-    @attempts = @current_user.attempts.where("`guessed_name` != 'unfair'") #aggregate 
+    @incorrect_attempts = @curr_attempts.select{|a| !a.correct}
+    @attempts = @current_user.attempts.where("`guessed_name` != 'unfair'") #aggregate
 
     # add friend count if missing
     begin
-      if @current_user.friend_count.nil? || @current_user.friend_count < 10 
+      if @current_user.friend_count.nil? || @current_user.friend_count < 10
         @current_user.friend_count =  @fb_graph.fql_query("SELECT friend_count FROM user WHERE uid = me()")[0]["friend_count"]
         @current_user.save!
       else
       	#Update the friend change so we can track...DO NOT UPDATE THE ORIGINAL NUMBERS!
       	@current_user.friend_change = @fb_graph.fql_query("SELECT friend_count FROM user WHERE uid = me()")[0]["friend_count"].to_i - @current_user.friend_count
-      	@current_user.save!      
+      	@current_user.save!
       end
 	  rescue
 	  end
@@ -213,7 +211,7 @@ class GameController < ApplicationController
     #computer user scores
     @my_score = @correct_attempts.length.to_f / (@correct_attempts.length + @incorrect_attempts.length) rescue 0
     @my_score = @my_score.nan? ? 1 : @my_score #above rescue is NOT catching, very odd error.
-    
+
     @my_agg_score = @attempts.select{|a| a.correct}.length.to_f / (@attempts.length)
     @fof_count =  (@current_user.friend_count * 880 * 0.5).round
 
@@ -231,7 +229,7 @@ class GameController < ApplicationController
           :accuracy => affil_accuracy.to_s
         }
       }
-	  rescue 
+	  rescue
 	  end
   end
 end
